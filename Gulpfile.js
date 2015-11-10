df9f3675257d50abb360fee21351c060cd0720c6
Chris <chrisaukema@gmail.com>
2015-11-10 15:53:20 +0100

    gulp test

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp');
 
+gulp.task('default', ['test','express'], function() {
+	
+});
+
 gulp.task('express', function() {
 	var express = require('express');
 	var app = express();
@@ -7,14 +11,9 @@ gulp.task('express', function() {
 	app.listen(process.env.PORT || 3000);
 });
 
-gulp.task('default', ['express'], function() {
-
-});
-
-gulp.task('serveprod', function() {
-  connect.server({
-    root: [your_project_path],
-    port: process.env.PORT || 5000, // localhost:5000
-    livereload: false
-  });
+gulp.task('test', function (done) {
+  new Server({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done).start();
 });
